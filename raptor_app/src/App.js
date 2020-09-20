8e476f11ae7d863d15fe5f06f819a5fa3d31564e
alannanoguchi <alanna.noguchi@gmail.com>
2020-09-20 11:42:20 -0700

    upload images button

--- raptor_app/src/App.js
+++ raptor_app/src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Landing from './Landing';
+import Landing from './components/Landing';
+import MultipleImageUploadComponent from './components/multiple-image-upload';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Landing />
+        <div className='container'>
+          <div className='row'>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">
+                  Multiple Image Upload Preview
+                </div>
+                <div className="card-body">
+                  <MultipleImageUploadComponent />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ raptor_app/src/components/multiple-image-upload.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class MultipleImageUploadComponent extends Component {
+
+    fileObj = [];
+    fileArray = [];
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            file: [null]
+        }
+        this.uploadMultipleFiles = this.uploadMultipleFiles.bind(this)
+        this.uploadFiles = this.uploadFiles.bind(this)
+    }
+
+    uploadMultipleFiles(e) {
+        this.fileObj.push(e.target.files)
+        for (let i = 0; i < this.fileObj[0].length; i++) {
+            this.fileArray.push(URL.createObjectURL(this.fileObj[0][i]))
+        }
+        this.setState({ file: this.fileArray })
+    }
+
+    uploadFiles(e) {
+        e.preventDefault()
+        console.log(this.state.file)
+    }
+
+    render() {
+        return (
+            <form>
+                <div className="form-group multi-preview">
+                    {(this.fileArray || []).map(url => (
+                        <img src={url} alt="..." />
+                    ))}
+                </div>
+
+                <div className="form-group">
+                    <input type="file" className="form-control" onChange={this.uploadMultipleFiles} multiple />
+                </div>
+                <button type="button" className="btn btn-danger btn-block" onClick={this.uploadFiles}>Upload</button>
+            </form >
+        )
+    }
+}
