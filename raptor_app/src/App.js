de341944f1c6d8ba6a0dc4979b64e6321dbf13ae
alannanoguchi <alanna.noguchi@gmail.com>
2020-10-18 21:59:19 -0700

    organization

--- raptor_app/src/App.js
+++ raptor_app/src/App.js
@@ -1,44 +1,22 @@
 import React, {Component} from 'react';
-// import logo from './logo.svg';
+
 import './App.css';
 import Landing from './components/Landing';
-import MultipleImageUploadComponent from './components/multiple-image-upload';
+import Appa from './components/image-upload/Appa'
 // import { render } from 'react-dom';
-import Buttons from './components/Buttons';
-import { Login } from './components/login';
+
+
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Landing />
-        <div className='container'>
-          <div className='row'>
-            <div className="col-md-6">
-              <div className="card">
-                <div className="card-header">
-                  Multiple Image Upload Preview
-                </div>
-                <div className="card-body">
-                  <MultipleImageUploadComponent />
-                </div>
-              </div>
-            </div>
-          </div>
+      // <Router>
+        <div className="App">
+          <Landing />
+          <Appa />
         </div>
-        <div className='container'>
-          <div className='row'>
-              <div className="col-md-6">
-                <div className="card">
-                  <div className="card-header">
-                    <Buttons />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      // </Router>
     );
   }
 }
--- raptor_app/src/components/Landing.js
+++ raptor_app/src/components/Landing.js
@@ -6,6 +6,12 @@ function Landing() {
     <div className="Header">
       <header>
         <h1>Anomaly Detection for Solar Panels</h1>
+        {/* <div>
+          <NavLink
+            className='nav-link'
+            activeClassName='nav-link-active'
+            exact to='/signin'>Sign In</NavLink>
+        </div> */}
       </header>
     </div>
   );
